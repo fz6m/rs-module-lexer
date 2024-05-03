df6ba8c9bb7e1c343d930a28e9b572c40ad7e125
fz6m <59400654+fz6m@users.noreply.github.com>
2024-05-04 06:16:30 +0900

    chore: add wasm update dts script

---
+++ scripts/wasmDts.ts
@@ -0,0 +1,42 @@
+import 'zx/globals'
+
+const run = async () => {
+  const root = path.join(__dirname, '../')
+  const wasmDTS = path.join(root, 'target/wasm/index.d.ts')
+  if (!fs.existsSync(wasmDTS)) {
+    throw new Error('index.d.ts not found, plz build wasm first')
+  }
+
+  const originContent = await fs.readFile(wasmDTS, 'utf-8')
+  const newLines: string[] = []
+  const lines = originContent.split('\n')
+  lines.forEach((line) => {
+    if (line.startsWith('export type ImportType')) {
+      // delete
+      return
+    } else if (line.includes(`ImportType`)) {
+      line = line.replace('ImportType', [1, 2, 3, 4, 5].join(' | '))
+      newLines.push(line)
+    } else {
+      // push
+      newLines.push(line)
+    }
+  })
+  const newContent = newLines.join('\n')
+
+  const wasmOutDir = [
+    path.join(root, 'target/wasm'),
+    path.join(root, 'target/wasm_web'),
+  ]
+  const tasks = wasmOutDir.map(async (dir) => {
+    const indexDtsPath = path.join(dir, 'index.d.ts')
+    if (!fs.existsSync(indexDtsPath)) {
+      // ignore
+    }
+    await fs.writeFile(indexDtsPath, `${newContent}\n`, 'utf-8')
+    console.log(`Update dts ${path.relative(root, indexDtsPath)}`)
+  })
+  await Promise.all(tasks)
+}
+
+run()
