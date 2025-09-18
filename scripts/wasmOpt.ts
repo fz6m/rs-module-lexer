f1e2dcfad64d62a8341f4cf9f607fba7e62bbab3
fz6m <59400654+fz6m@users.noreply.github.com>
2025-09-19 03:48:03 +0900

    fix: wasm build configs

--- scripts/wasmOpt.ts
+++ scripts/wasmOpt.ts
@@ -5,25 +5,24 @@ import 'zx/globals'
 const platform = os.platform()
 const arch = os.arch()
 
-// https://github.com/rustwasm/wasm-pack/issues/864#issuecomment-957818452
 const run = async () => {
-  const version = 'version_116'
+  const version = 'version_124'
   const platformMark =
     platform === 'darwin'
       ? 'macos'
       : platform === 'linux'
-      ? 'linux'
-      : (() => {
-          throw new Error('Unsupported platform')
-        })()
+        ? 'linux'
+        : (() => {
+            throw new Error('Unsupported platform')
+          })()
   const archMark =
     arch === 'x64'
       ? 'x86_64'
       : arch === 'arm64'
-      ? 'arm64'
-      : (() => {
-          throw new Error('Unsupported arch')
-        })()
+        ? 'arm64'
+        : (() => {
+            throw new Error('Unsupported arch')
+          })()
   const url = `https://github.com/WebAssembly/binaryen/releases/download/${version}/binaryen-${version}-${archMark}-${platformMark}.tar.gz`
   console.log(`Platform: ${platform}`)
   console.log(`Arch: ${arch}`)
@@ -49,7 +48,7 @@ const run = async () => {
     `./.cache/binaryen-${version}/bin/wasm-opt`,
   )
   const dir = argv.dir || 'wasm'
-  await $`${optPath} -Oz -o ./target/${dir}/index_bg.wasm ./target/${dir}/index_bg.wasm`
+  await $`${optPath} -Oz --enable-bulk-memory --enable-nontrapping-float-to-int --merge-similar-functions --converge -o ./target/${dir}/index_bg.wasm ./target/${dir}/index_bg.wasm`
 }
 
 run()
